6a74a595a6ad697408ac02f52e89e9e6d4a76a90
贝密游戏 <open@ukewo.cn>
2017-08-08 20:49:20 +0800

    no commit message

--- src/main/java/com/beimi/config/web/GameServerConfiguration.java
+++ src/main/java/com/beimi/config/web/GameServerConfiguration.java
@@ -76,8 +76,7 @@ public class GameServerConfiguration
 //		config.setStoreFactory(new HazelcastStoreFactory());
 		config.setAuthorizationListener(new AuthorizationListener() {
 			public boolean isAuthorized(HandshakeData data) {
-				String token = data.getSingleUrlParam("token") ; 
-				return !StringUtils.isBlank(token);	//其他安全验证策略，IP，Token，用户名
+				return true;	//其他安全验证策略，IP，Token，用户名
 			}
 		});
 		/**
