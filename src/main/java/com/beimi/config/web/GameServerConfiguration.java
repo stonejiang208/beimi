f6f0637a8f0e50515edef3fccf430efc15a0551b
贝密游戏 <open@ukewo.cn>
2017-10-25 08:39:58 +0800

    no commit message

--- src/main/java/com/beimi/config/web/GameServerConfiguration.java
+++ src/main/java/com/beimi/config/web/GameServerConfiguration.java
@@ -84,7 +84,6 @@ public class GameServerConfiguration
 		/**
 		 * 性能优化
 		 */
-		config.setPingInterval(5000);	//
 		config.getSocketConfig().setReuseAddress(true);
 		config.getSocketConfig().setSoLinger(0);
 		config.getSocketConfig().setTcpNoDelay(true);
